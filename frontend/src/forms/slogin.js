fd9f808953c5fbf5419930356be161eb15d30e6d
kasettyvenkatasai <venkatsaikp7@gmail.com>
2025-05-05 21:20:34 +0530

    rsgvrcgjdc1nj

--- frontend/src/forms/slogin.js
+++ frontend/src/forms/slogin.js
@@ -3,7 +3,6 @@ import "./dslogin.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
-import {Cookies} from 
 function Slogin() {
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState("");
